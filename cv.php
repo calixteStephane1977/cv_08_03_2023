d0eb20231a8d7cede5d92039c925c9af69681290
calixte <calixtedeveloppeur@gmail.com>
2023-03-08 20:20:58 +0100

    push cv

---
+++ cv.php
@@ -0,0 +1,25 @@
+$Entreprise=htmlspecialchars($_POST["entreprise"]);
+$Adresse=htmlspecialchars($_POST["adresse"]);
+$Cite=htmlspecialchars($_POST["cite"]);
+$Code_postale=htmlspecialchars($_POST["$Code_postal"]);
+$Phone=htmlspecialchars($_POST["$phone"]);
+$Textarea=htmlspecialchars($_POST["$textarea"]);
+$Adresse=htmlspecialchars($_POST["$adresse"]);
+
+echo "Bonjour".$Entreprise.'!';
+
+$monfichier=fopen('donneesformulaire.txt,'a+');
+
+fputs($monfichier,"==========================\n");
+fputs($monfichier,$Entreprise."\n");
+fputs($monfichier,$Adresse."\n");
+fputs($monfichier,$Cite."\n");
+fputs($monfichier,$Code."\n");
+fputs($monfichier,$Phone."\n");
+fputs($monfichier,$Textarea."\n");
+fputs($monfichier,$Adresse."\n");
+
+fclose($monfichier);
+
+
+
